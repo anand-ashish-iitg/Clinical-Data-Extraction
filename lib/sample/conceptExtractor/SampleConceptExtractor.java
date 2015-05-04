1559d1ce43ac27a779f27aaf73852b6143164c00
amit-mittal <amit.mittal1993@gmail.com>
2015-05-04 14:04:51 +0530

    Basic concept extractor working

--- lib/sample/conceptExtractor/SampleConceptExtractor.java
+++ lib/sample/conceptExtractor/SampleConceptExtractor.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import banner.Sentence;
 import banner.tagging.CRFTagger;
 import banner.tagging.Mention;
+import banner.tagging.TaggedToken;
+import banner.tagging.TaggedToken.TagFormat;
+import banner.tokenization.Token;
 
 import com.care.exception.ComponentException;
 import com.care.framework.IConceptExtractor;
@@ -21,32 +25,49 @@ public class SampleConceptExtractor implements IConceptExtractor
     public List<String> ExtractConcepts(String data) throws ComponentException
     {
         String crfLocation;
+        List<String> taggedText = new ArrayList<String>();
         try
         {
             crfLocation = URLDecoder.decode(getClass().getResource("CRF_Model.txt").getFile().toString(), "UTF-8");
             File file = new File(crfLocation);
         
-            Sentence s = Sentence.loadFromPiped(null, "inguinal|O adenopathy|O was|O no|O appreciable|O cervical|O ,|O supraclavicular|O ,|O axillary|O ,|O or|O inguinal|O adenopathy|O .|O ");
+            Sentence s = Sentence.loadFromPiped(null,   "The|B-test review|I-test of|I-test systems|I-test is|O negative|O ,|O except|O as|O above|O .|O \n" +
+                                                        "ALLERGIES|O :|O \n" +  
+                                                        "The|O patient|O has|O no|O known|B-problem drug|I-problem allergies|I-problem .|O  ");
             CRFTagger t = CRFTagger.load(file, null, null);
             t.tag(s);
-            List<Mention> m = s.getMentions();
-            for (Mention mention : m)
+            
+            List<Token> tokens = s.getTokens();
+            int size = tokens.size();
+            for(int index = 0; index < size; ++ index)
             {
-                System.out.println(mention);
+                Token token = tokens.get(index);
+                String text = token.getText();
+                boolean ifConcept = (s.getMentions(token).size() > 0);
+                
+                if(ifConcept)
+                {
+                    Mention m = s.getMentions(token).get(0);
+                    String tag = m.getType().getText();
+                    taggedText.add("<"+tag+">" + m.getText() +"</"+tag+">");
+                    while(m.contains(index))
+                    {
+                        ++index;
+                    }
+                    --index;
+                }
+                else
+                {
+                    taggedText.add(text);
+                }
             }
         }
-        catch (UnsupportedEncodingException e)
+        catch (Exception e)
         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new ComponentException(e);
         }
         
-        return null;
+        return taggedText;
     }
 
     public List<String> ExtractConcepts(List<String> data) throws ComponentException
